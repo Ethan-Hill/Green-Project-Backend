8e1bfdfb60a838ea167e5846193532df8dc6dbc0
Ethan-Hill <ethanhilljay@outlook.com>
2021-04-23 03:19:06 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
 const { authCode } = require("../utils")
-
+require("dotenv").config()
 const nodemailer = require("nodemailer")
 const mg = require("nodemailer-mailgun-transport")
 
