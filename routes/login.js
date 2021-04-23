82539f1ce8fdda768fc8a18892601e7099193306
Ethan-Hill <ethanhilljay@outlook.com>
2021-04-23 10:19:52 +0100

    that didnt work

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,6 @@ const { authCode } = require("../utils")
 require("dotenv").config()
 const nodemailer = require("nodemailer")
 const mg = require("nodemailer-mailgun-transport")
-var htmlToText = require("nodemailer-html-to-text").htmlToText
 
 const auth = {
   auth: {
@@ -32,7 +31,6 @@ router.post("/", async (req, res) => {
     const code = authCode()
 
     await User.findOneAndUpdate({ email: data.email }, { $set: { code: code } })
-    nodemailerMailgun.use("compile", htmlToText())
     nodemailerMailgun.sendMail(
       {
         from: "verify@ethanhill.dev",
